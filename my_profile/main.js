56f2f1d2a53f6e9e573c5d8f3cb2b26f0fc8902f
Simeon Kenneth T <51843241+KenRelic@users.noreply.github.com>
2020-04-18 14:12:44 +0100

    Update main.js

--- my_profile/main.js
+++ my_profile/main.js
@@ -3,7 +3,6 @@ window.onload = () => {
     let contact_form = document.getElementById('contact-form');
     contact_form.style.bottom = '-105vh';
     create_message_btn.addEventListener('click', () => {
-        console.log(contact_form.style.bottom)
         if (contact_form.style.bottom == '-105vh') {
             contact_form.style.bottom = '0';
         } else {
