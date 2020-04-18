d3bf6dd7e99264beca8b1be49d40f223245077ba
Simeon Kenneth T <51843241+KenRelic@users.noreply.github.com>
2020-04-18 14:09:56 +0100

    Update main.js

--- my_profile/main.js
+++ my_profile/main.js
@@ -1,15 +1,19 @@
-let send_message_btn = document.querySelector('#contact-user span:nth-of-type(2)');
-let contact_form = document.getElementById('contact-form');
-send_message_btn.addEventListener('click', () => {
-    console.log(contact_form.style.bottom)
-    if (contact_form.style.bottom == '-105vh') {
-        contact_form.style.bottom = '0';
-    } else {
-        contact_form.style.bottom = '-105vh';
-    }
-});
-
-let close_btn = document.getElementById('close-btn');
-close_btn.addEventListener('click', ()=>{
+window.onload = () => {
+    let create_message_btn = document.querySelector('#contact-user >button');
+    let contact_form = document.getElementById('contact-form');
     contact_form.style.bottom = '-105vh';
-})
+    create_message_btn.addEventListener('click', () => {
+        console.log(contact_form.style.bottom)
+        if (contact_form.style.bottom == '-105vh') {
+            contact_form.style.bottom = '0';
+        } else {
+            contact_form.style.bottom = '-105vh';
+        }
+    });
+
+    let close_btn = document.getElementById('close-btn');
+    close_btn.addEventListener('click', () => {
+        contact_form.style.bottom = '-105vh';
+    })
+}
+
